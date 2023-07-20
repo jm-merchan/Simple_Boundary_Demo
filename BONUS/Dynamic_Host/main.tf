479319717ed47ce94c4fbb5992f5ab155d481416
jm-merchan <jose.merchan@hashicorp.com>
2023-07-20 18:17:21 +0200

    moving some stuff over aws.tf

--- BONUS/Dynamic_Host/main.tf
+++ BONUS/Dynamic_Host/main.tf
@@ -12,46 +12,6 @@ terraform {
     }
   }
 }
-/*
-output "boundary_access_key_id" {
-    value = aws_iam_access_key.boundary.id
-}
-
-output "boundary_secret_access_key" {
-  value = aws_iam_access_key.boundary.secret
-  sensitive = true
-}
-
-
-
-resource "aws_iam_user" "boundary" {
-  name = "boundary"
-  path = "/"
-}
-
-resource "aws_iam_access_key" "boundary" {
-  user = aws_iam_user.boundary.name
-}
-
-resource "aws_iam_user_policy" "BoundaryDescribeInstances" {
-  name = "BoundaryDescribeInstances"
-  user = aws_iam_user.boundary.name
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:DescribeInstances"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-*/
 
 
 
